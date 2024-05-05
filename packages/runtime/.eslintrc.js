138332cf876e2c11bf78a5a0dd47c46cebca2b53
Punit Jain <contactpunitjain@gmail.com>
2024-05-05 22:24:59 +0530

    project layout for custom web framework

---
+++ packages/runtime/.eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+    env: {
+        browser: true,
+        es2021: true
+    },
+    extends: 'eslint:recommended',
+    overrides: [],
+    parseOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module'
+    },
+    rules: {}
+}
