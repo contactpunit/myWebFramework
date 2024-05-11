488b867f4f9ba5b6ba786672ee6f01742bb5e1dd
Punit Jain <contactpunitjain@gmail.com>
2024-05-11 15:14:37 +0530

    initial project ;ayout using vanila JS

---
+++ examples/todoapp/todo.js
@@ -0,0 +1,6 @@
+const todos = ['study JS', 'proactive Nodejs', 'create framework']
+
+const addTodoInputEl = document.querySelector('#todo-input')
+const addTodoBtn = document.querySelector('button')
+const todoList = document.querySelector('#todos-list')
+console.log(addTodoBtn, addTodoInputEl, todoList)
